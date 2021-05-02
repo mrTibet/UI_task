5d79cc5accd5a03085613322e2e80cc5ea76f4ae
MrTibet <vader.ctylxy@gmail.com>
2021-05-02 22:41:22 +0300

    chane script

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 let bar = document.querySelector('.bars'),
-    menu = document.querySelector('.menu');
+    closeMenu = document.querySelector('.close'),
+    menu = document.querySelector('.menu')
 
 //overlay menu
 
-bar.addEventListener('click', function(){
+/*bar.addEventListener('click', function(){
     menu.style.display = 'block';
+    bar.classList.toggle('passive')
 });
 
 
-menu.addEventListener('click', function(){
+closeMenu.addEventListener('click', function(){
     menu.style.display = 'none';
-})
+    bar.classList.toggle('passive')
+})*/
+
+function openClose(){
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
+
+bar.addEventListener('click', openClose)
+closeMenu.addEventListener('click', openClose)
+
